6d99eeb79dc553bdfbf03a420c6a2b27609f3210
IceColdDude759 <anirbanmajumder2021@gmail.com>
2022-01-01 20:47:26 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,65 @@
+import speech_recognition as sr
+import pyttsx3
+import time
+import webbrowser
+
+engine=pyttsx3.init('sapi5')
+voices=engine.getProperty('voices')
+engine.setProperty('voice','voices[1].id')
+
+
+
+def get_input():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+            print("say")
+            audio = r.listen(source)
+    try:            
+        print("processing............")
+        text = r.recognize_google(audio,language='en-in')
+        print (text)
+        return text
+    except  :
+        print("failed")
+        return None
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def process(statement):
+    if 'open youtube' in statement:
+        webbrowser.open_new_tab("https://www.youtube.com")
+        speak("youtube is open now")
+        time.sleep(5)
+
+    elif 'open google' in statement:
+        webbrowser.open_new_tab("https://www.google.com")
+        speak("Google is open now")
+        time.sleep(5)
+
+    elif 'open gmail' in statement:
+        webbrowser.open_new_tab("gmail.com")
+        speak("Google Mail open now")
+        time.sleep(5)
+
+    elif 'open vlc' in statement:
+        pass
+
+    
+
+
+
+
+
+if __name__=='__main__':
+
+
+    while True:
+        speak("Tell me how can I help you now?")
+        a=input("Enter to continue")
+        statement = get_input()
+        if statement==None:
+            break
+        statement = statement.lower()
+        process(statement)
